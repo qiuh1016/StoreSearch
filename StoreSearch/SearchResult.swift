380b919514e1e0a28611159ebf18c5405f2d4d1b
qiuh1016 <qiuh1016@126.com>
2016-01-29 13:07:54 +0800

    Added a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by 裘鸿 on 1/26/16.
+//  Copyright © 2016 CETCME. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
